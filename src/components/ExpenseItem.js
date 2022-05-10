43a621fd751990908614592a01f9d244c60fa66a
Tejvir <tejvirdhami99@gmail.com>
2022-05-10 09:49:48 -0400

    Add reusable component ExpenseItem.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,23 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  //   const expenseDate = new Date(2022, 2, 24);
+  //   const expenseTitle = "Car Insurance";
+  //   const expenseAmount = 294.67;
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+
   return (
-    <div>
-      <div>Date</div>
+    <div className="expense-item">
       <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
