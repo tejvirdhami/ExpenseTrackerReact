740149f9e34df45920eab16c3d7175b5a4f8c00a
Tejvir <tejvirdhami99@gmail.com>
2022-05-12 15:29:22 -0400

    Add User Input and Filter component.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -8,25 +8,13 @@ function ExpenseItem(props) {
   //   const expenseTitle = "Car Insurance";
   //   const expenseAmount = 294.67;
 
-  //second input is function to change title
-  const [title, setTitle] = useState(props.title);
-
-  // let title = props.title;
-
-  function clickHandler() {
-    //We wont assign values by assignment but by calling a func from use state!
-    setTitle("Updated!");
-    console.log(title);
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
