9e8ae91070d074bab3b3d5f451a69f60ae91419c
Tejvir <tejvirdhami99@gmail.com>
2022-05-12 14:11:48 -0400

    Design User Input and store entered Values.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,13 @@
 import React from "react";
-import ".NewExpense.css";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 
 function NewExpense() {
-  return <div></div>;
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
 }
 
 export default NewExpense;
